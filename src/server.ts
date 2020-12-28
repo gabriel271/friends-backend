c5be96b93ecddb0cab3b7d2063f160010b99f9d9
gabriel271 <gabrielsantanasilva85@gmail.com>
2020-12-28 19:29:15 -0300

    deploy 2

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,12 @@
-import express from 'express';
 import socketIO, { Socket } from 'socket.io';
-import http from 'http';
-import cors from 'cors';
 import userEventsIO from './events/userEventsIO';
 import Room from './entities/room';
 import roomEventsIO from './events/roomEventsIO';
 import User from './entities/user';
 import messageEventsIO from './events/messageEventsIO';
 
-const HOST = process.env.HOST || 'localhost';
 const PORT = process.env.PORT || 8080;
-const app = express();
-app.use(cors());
-const server = http.createServer(app);
-const io = new socketIO.Server(server, {cors: {origin: '*'}});
+const io = new socketIO.Server(PORT as number, {cors: {origin: '*'}});
 const users: User[] = [];
 const rooms: Room[] = [];
 
@@ -26,4 +19,3 @@ io.on('connection', (socket: Socket) => {
     roomEventsIO.joinRoom(rooms, socket, users);
     messageEventsIO.sentMesage(socket, users, rooms);
 });
-server.listen(PORT as number, HOST, () => console.log(`server running: http://${HOST}:${PORT}`));
